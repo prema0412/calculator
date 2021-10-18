9c97634cc546f16e23c50a4062ce74607fe21a78
unknown <prema.chhaya@gmail.com>
2021-10-18 21:27:58 +0530

    all operations implemented trouble shooting in progress

--- calc.js
+++ calc.js
@@ -1,10 +1,12 @@
 
 
 
-let previousNUmber = ''
+let previousNUmber = '0'
 let currentNumber = ''
 let displayNumber = "0"
 let enteredNumber = ''
+let previousResult = '0'
+let currentResult = ''
 
 
 //function to display entered numbers on output panel
@@ -19,33 +21,234 @@ const displayInputs = ( evnt => {
     
     enteredNumber = evnt.target.innerHTML
 
-    console.log(enteredNumber);
 
     if (displayNumber == 0)
     {
         displayNumber = ''
     }   
+
+    // User shall not enter more than one decimal
     
     if ( displayNumber.includes('.')  && (enteredNumber == '.') )
     {
-        Aler("I am checking decimals")
         return
     }
+
+
+    // User shall not enter more than 9 digits at a time
+
+    if (displayNumber.length == 9)
+    {
+        return
+    }
+   
+        displayNumber += enteredNumber
+        previousResult = displayNumber
+
+        console.log(displayNumber);
+        console.log(previousResult);
     
-    displayNumber += enteredNumber
-    currentNumber = enteredNumber
-    previousNUmber += enteredNumber
+    
+    // displayNumber += enteredNumber
+    // currentNumber = displayNumber
+    // previousNUmber = displayNumber
+    // previousResult = displayNumber
+
     let outPut = document.getElementById('display')
 
     outPut.innerHTML = displayNumber
 
+    // console.log(typeof(displayNumber))
+
 
 })
+
+//function to add sign (negative numbers are allowed)
+
+const addSign = () => {
+    
+        console.log(displayNumber + " " + Math.abs(displayNumber));
+
+       
+       
+        switch (Math.sign(displayNumber)) {
+            case -1:
+            case -0:    
+                break;
+            case 1:
+            case 0:  
+                 displayNumber = (-Math.abs(displayNumber)).toString() 
+
+            default:
+                break;
+        }
+        outPut.innerHTML = displayNumber
+
+}
+
+// const addDecimal = () => {
+    
+
+//     console.log(displayNumber);
+
+   
+   
+//     switch (toString(displayNumber).includes('.')) {
+//         case true:    
+//             break;
+//         case false:
+//              displayNumber = displayNumber + document.getElementById('decimaml') 
+//         default:
+//             break;
+//     }
+//     outPut.innerHTML = displayNumber
+// }
+    
 
 //function to perform calculation
 
 const performcalculation = ( evnt => {
-    alert("I will calculate soon")
+
+    selectedOperator = evnt.target.innerHTML
+
+    console.log(selectedOperator);
+    switch (selectedOperator) {
+        case '+':
+
+            console.log(displayNumber);
+            console.log(previousResult);
+
+            if ( previousResult == 0)
+
+            {
+                previousResult = displayNumber
+                displayNumber = 0
+            }
+            else {
+                previousResult = parseFloat(previousResult) + parseFloat(displayNumber)
+                displayNumber = 0
+            }
+            console.log(displayNumber)
+            console.log(previousResult)
+            outPut.innerHTML = previousResult
+            break;
+
+            case '-':
+
+                console.log(displayNumber);
+                console.log(previousResult);
+    
+
+                if ( previousResult == 0)
+    
+                {
+                    previousResult = displayNumber
+                    displayNumber = 0
+                }
+                else {
+                    previousResult = parseFloat(previousResult) - parseFloat(displayNumber)
+                    displayNumber = 0
+                }
+                console.log(displayNumber)
+                console.log(previousResult)
+                outPut.innerHTML = previousResult
+                break;
+
+            case 'x':
+
+            console.log(displayNumber);
+            console.log(previousResult);
+
+            if ( previousResult == 0)
+
+
+            
+            {
+                previousResult = displayNumber
+                displayNumber = 0
+            }
+            else {
+
+                if  ( (displayNumber != 0 ) && (previousResult != displayNumber) ) {
+
+                    previousResult = parseFloat(previousResult) * parseFloat(displayNumber)
+                    displayNumber = 0
+                }
+               
+            }
+            console.log(displayNumber)
+            console.log(previousResult)
+            outPut.innerHTML = previousResult
+            break;
+
+            case '÷':
+
+            console.log(displayNumber);
+            console.log(previousResult);
+
+            if ( previousResult == 0)
+
+            {
+                previousResult = displayNumber
+                previousNUmber = displayNumber
+                displayNumber = 0
+            }
+            else {
+
+                if  (!(displayNumber == 0 ) ) {
+                    
+                    previousResult = parseFloat(previousResult) / parseFloat(displayNumber)
+                    displayNumber = 0
+                }
+               
+            }
+            console.log(displayNumber)
+            console.log(previousResult)
+            outPut.innerHTML = previousResult
+            break;
+
+
+            case '%':
+
+                console.log(displayNumber);
+                console.log(previousResult);
+    
+                if ( previousResult == 0) previousResult = displayNumber
+    
+                
+                    
+                previousResult = (parseFloat(previousResult) / 100)
+                displayNumber = 0
+
+                console.log(displayNumber)
+                console.log(previousResult)
+                outPut.innerHTML = previousResult
+                break;    
+
+                case '=':
+
+                    console.log(displayNumber);
+                    console.log(previousResult);
+        
+    
+                    if ( previousResult == 0)
+        
+                    {
+                        previousResult = displayNumber
+                        displayNumber = 0
+                    }
+                    // else {
+                    //     previousResult = parseFloat(previousResult) - parseFloat(displayNumber)
+                    //     displayNumber = 0
+                    // }
+                   
+                    outPut.innerHTML = previousResult
+                    displayNumber = 0
+                    break;    
+    
+        default:
+            break;
+    }
 })
 
 
@@ -78,6 +281,42 @@ for ( i=0; i<operators.length; i++)
 
     
 }
+
+let outPut = document.getElementById('display')
+
+//Implementing clear and all clear function on click event
+
+let clearOption = document.getElementById('clear')
+clearOption.addEventListener('click', () => {
+
+        if (clearOption.innerHTML == "AC") {
+            displayNumber = 0
+
+            previousNUmber = ''
+            previousResult = 0
+            currentNumber = ''
+            // console.log(previousNUmber);
+        }
+
+        if(clearOption.innerHTML == "C") {
+
+            displayNumber = 0
+            outPut.innerHTML = displayNumber
+            clearOption.innerHTML = "AC"
+            // console.log(previousNUmber);
+
+            // console.log(displayNumber);
+            // let lengthOfDisplay = displayNumber.length
+            // // console.log(lengthOfDisplay);
+            // displayNumber = displayNumber.slice(0, lengthOfDisplay-1)
+            // outPut.innerHTML = displayNumber
+
+        }
+})
+
+
+
+
 
 
 
